62bd380d2c54a0ff7b539d22205e1ea6a1af1b36
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-03-25 19:25:09 +0100

    Use chef_gem to be able to use ini-file at compile time

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,10 +18,11 @@
 #
 
 include_recipe 'gem_specific_install'
-# chef_gem "inifile" do
-#  provider Chef::Provider::Package::Rubygems::SpecificInstall
-#  options( :repo => "https://github.com/optiflows/inifile.git")
-# end
+
+chef_gem "inifile" do
+ provider Chef::Provider::Package::Rubygems::SpecificInstall
+ options( :repo => "https://github.com/optiflows/inifile.git")
+end
 
 gem_specific_install 'ini-phile' do
   repository 'https://github.com/guilhem/ini-phile.git'
